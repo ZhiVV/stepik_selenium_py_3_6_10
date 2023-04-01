d99f05282de703d76449050cc9c248f6f13052a9
Vlad Zhichkin <vlad.zhichkin@gmail.com>
2023-04-01 18:32:11 +0500

    Remove WebDriverWait and changet assert

--- test_items.py
+++ test_items.py
@@ -1,15 +1,12 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 
 def test_name_button_add_to_basket(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/') 
-    time.sleep(30)
-    button = WebDriverWait(browser, 20).until(
-        EC.presence_of_element_located((By.CLASS_NAME, 'btn-add-to-basket'))
-    )
+    time.sleep(10)
     
-    assert button != None, 'Button "Add to basket" not found.'
+    button = browser.find_elements(By.CLASS_NAME, 'btn-add-to-basket')
+    assert len(button) > 0, 'Button "Add to basket" not found.'
+    assert len(button) < 2, 'Button "Add to basket" more than one.'
